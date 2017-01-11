e826d1aa6d93f4a9ed575cdfcefbe630ee2addb5
Ceci-PC\Ceci <tsvetelinkostadinovts@gmail.com>
2017-01-11 15:52:41 +0200

    initializing the map is now a separate function added opening mechanic numbers of row and colomns

--- src/minesweeper.java
+++ src/minesweeper.java
@@ -2,9 +2,11 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class minesweeper {
+	
 	public static int[][] initializeMap(int howManyMines, int[][] emptyMap)
 	{
 		Random random = new Random();
+		
 		for(int i=0; i<emptyMap.length;i++)
 		{
 			emptyMap[i][0] = -2;
@@ -38,34 +40,6 @@ public class minesweeper {
 		return emptyMap;
 	}
 	
-	public static void showMap(boolean[][] cover, int[][] map)
-	{
-		for(int i=0; i<map.length; i++)
-		{
-			for(int j=0; j<map[0].length; j++)
-			{
-				if(map[i][j]==-2)
-				{
-					System.out.print("# ");
-				}else if(!cover[i][j])
-				{
-					if(map[i][j]==-1)
-					{
-						System.out.print("X ");
-						
-					}else
-					{
-						System.out.print(minesAround(i, j, map)+" ");
-					}
-				}else{
-					System.out.print("[]");
-				}
-			}
-			System.out.println();
-		}
-		
-	}
-	
 	public static int minesAround(int x,int y,int[][] map)
 	{
 		int count=0;
@@ -82,36 +56,128 @@ public class minesweeper {
 		return count;
 	}
 	
+	public static void showMap(int[][] cover, int[][] map)
+	{
+		System.out.print(" ");
+		for(int i=0;i<map[0].length; i++)
+		{
+			if(i<10)
+			{
+				System.out.print(" "+i+" ");
+			}else{
+				System.out.print(i+" ");
+			}	
+		}
+		System.out.println();
+		for(int i=0; i<map.length; i++)
+		{
+			System.out.print(i);
+			for(int j=0; j<map[0].length; j++)
+			{
+				if(map[i][j]==-2)
+				{
+					if(i<10 && j==0)
+					{
+						System.out.print(" # ");
+					}else if(j==0){
+						System.out.print("# ");
+					}else{
+						System.out.print(" # ");
+					}
+				}else if(cover[i][j]==1)
+				{
+					if(map[i][j]==-1)
+					{
+						System.out.print("[X]");
+						
+					}else
+					{
+						System.out.print("["+minesAround(i,j,map)+"]");
+					}
+				}else if(cover[i][j]==0)
+				{
+					System.out.print("[ ]");
+				}else
+				{
+					System.out.print("[M]");
+				}	
+			}
+			System.out.println();
+		}
+	}
+
+	
 	public static void main(String[] arg)
 	{
 		Scanner input = new Scanner(System.in);
-		
 		
 		System.out.print("How wide should it be:");
 		int rangeX = input.nextInt();
 		System.out.print("How high should it be:");
 		int rangeY = input.nextInt();
+
 		int[][] map = new int[rangeX][rangeY];
-		boolean[][] cover = new boolean[rangeX][rangeY];
+		int[][] cover = new int[rangeX][rangeY];
 		
-//		for(int i=0; i<map.length;i++)
-//		{
-//			for(int j=0; j<map[0].length;j++)
-//			{
-//				cover[i][j] = true;
-//			}
-//		}
-		
-		int countMines = rangeX*rangeY/4;
-		System.out.println("There are "+countMines+" mines");
+		boolean boom=false;
+		//for map
+		//-3 = marked
 		//-2 = border
 		//-1 = mine
 		//0 = empty
 		//number....
+		
+		//for cover
+		//-1=marked
+		//0=hidden
+		//1=open
+		for(int i=0; i<map.length;i++)
+		{
+			for(int j=0; j<map[0].length;j++)
+			{
+				cover[i][j] = 0;
+			}
+		}
+		int countMines = rangeX*rangeY/10;
+		int markedMines=0;
+		int marks=0;
+		System.out.println("There are "+countMines+" mines(1/10 of the field)");
+
 		map = initializeMap(countMines, map);
 		
 		
-		showMap(cover, map);
+		
+		while(!boom)
+		{
+			showMap(cover, map);
+			
+			System.out.print("What row is the cell in?");
+			int y=input.nextInt();
+			System.out.print("What colomn is the cell in?");
+			int x=input.nextInt();
+			System.out.println("This size includes the borders which are 1 cell big");
+			System.out.println("What to do with it?");
+			System.out.println("[1]Open");
+			System.out.println("[2]Mark");
+			int action=input.nextInt();
+			if(action == 1)
+			{
+				if(map[x][y]==-2)
+				{
+					System.out.println("You can't open a border");
+				}else if(map[x][y]==-1)
+				{
+					System.out.println("You opened a mine!!!");
+					boom=true;
+				}else if(cover[x][y]==-1){
+					System.out.println("You can't open a marked cell. Unmark it first.");
+				}else{
+					System.out.println("Successfully opened");
+					cover[x][y]=1;
+				}
+			}
+		
+		}
 		
 	}
 }
